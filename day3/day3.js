27475974c699b2d3cd01d331335d15c2362b8752
gcerame <g.cerame@gmail.com>
2022-12-03 20:33:15 +0100

    day 3

--- day3/day3.js
+++ day3/day3.js
@@ -7,22 +7,19 @@ const testInput = 'vJrwpWtwJgWrhcsFMMfFFhFp\n' +
     'ttgJtRGJQctTZtZT\n' +
     'CrZsJsPPZsGzwwsLwLmpwMDw';
 
-const input =
-    fs
-    .readFileSync('input.txt', 'utf-8')
-    .trim()
+const input = fs
+        .readFileSync('input.txt', 'utf-8')
+        .trim()
         .split('\n');
 
 const rucksacks = input.map((line) => {
     return [line.substring(0, line.length / 2), line.substring(line.length / 2)];
-
 });
-const findCommonItemInRucksack = (rucksack) => {
+const findCommonItemInRucksacks = (rucksacks) => {
     const commonItems = rucksacks.map(rucksack => rucksack[0].split('')
         .filter((char) => {
             return rucksack[1].indexOf(char) > -1;
         }));
-    //Return an array with only the first common item
     return commonItems.map(commonItem => commonItem[0]);
 };
 const calculatePriority = (item) => {
@@ -35,20 +32,16 @@ const calculatePriority = (item) => {
 
 const findCommonItemInGroup = (elfGroup) => {
     const firstRucksack = elfGroup[0].split('');
-    const commonItem = firstRucksack
+    return firstRucksack
         .filter((item) => elfGroup[1].indexOf(item) > -1 && elfGroup[2].indexOf(item) > -1)[0];
-
-    return commonItem;
 };
 
-const prioritiesPartOne = findCommonItemInRucksack(rucksacks).map(item => calculatePriority(item));
+const prioritiesPartOne = findCommonItemInRucksacks(rucksacks).map(item => calculatePriority(item));
 const totalPriorityPartOne = prioritiesPartOne.reduce((a, b) => a + b, 0);
 console.log('Part 1: ', totalPriorityPartOne);
 
-
 //Part 2
 const elfGroups = [];
-let count = 0;
 for (let i = 0; i < input.length; i = i + 3) {
     elfGroups.push([input[i], input[i + 1], input[i + 2]]);
 }
