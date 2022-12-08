813fb00d25580df0dd75d3706904e9a6a1f8e456
gcerame <g.cerame@gmail.com>
2022-12-08 22:09:11 +0100

    day 7 incomplete

---
+++ day7/day7.js
@@ -0,0 +1,79 @@
+const example = '$ cd /\n' +
+    '$ ls\n' +
+    'dir a\n' +
+    '14848514 b.txt\n' +
+    '8504156 c.dat\n' +
+    'dir d\n' +
+    '$ cd a\n' +
+    '$ ls\n' +
+    'dir e\n' +
+    '29116 f\n' +
+    '2557 g\n' +
+    '62596 h.lst\n' +
+    '$ cd e\n' +
+    '$ ls\n' +
+    '584 i\n' +
+    '$ cd ..\n' +
+    '$ cd ..\n' +
+    '$ cd d\n' +
+    '$ ls\n' +
+    '4060174 j\n' +
+    '8033020 d.log\n' +
+    '5626152 d.ext\n' +
+    '7214296 k';
+
+const splitInput = example.split('\n');
+console.log(splitInput);
+
+const createTree = (input) => {
+    const tree = {
+        name: '/',
+        isDirectory: true,
+        children: [],
+    };
+
+    let currentDirectory = tree;
+    let currentCommand = null;
+
+    for (let line of input) {
+        let splitLine = line.split(' ');
+        if (splitLine[0] === '$') { // command
+            currentCommand = splitLine[1];
+            if (currentCommand === 'cd') {
+                if (splitLine[2] === '..') {
+                    currentDirectory = currentDirectory.parent;
+                } else if (splitLine[2] === '/') {
+                    currentDirectory = tree;
+                } else { // go to child directory
+                    currentDirectory = currentDirectory.children.find(child => child.name === splitLine[2]);
+                }
+            }
+        } else if (currentCommand === 'ls') {
+            if (!isNaN(splitLine[0])) {
+                //File
+                currentDirectory.children.push({
+                    name: splitLine[1],
+                    isDirectory: false,
+                    parent: currentDirectory,
+                    size: parseInt(splitLine[0]),
+                });
+            } else {
+                //Directory
+                currentDirectory.children.push({
+                    name: splitLine[1],
+                    isDirectory: true,
+                    parent: currentDirectory,
+                    children: [],
+                });
+            }
+        }
+    }
+    return tree;
+
+};
+
+
+
+
+const fileTree = createTree(splitInput);
+console.log( fileTree );
